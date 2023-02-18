f8a975eb4b521d6a3ac7cb7dd66aeec409009604
Erol Dalkılıç <eroldalg@gmail.com>
2023-02-18 22:50:57 +0300

    datagridview

---
+++ Example-sql-project/Example-sql-project/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Example_sql_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.okulDataSet = new Example_sql_project.okulDataSet();
+            this.sayfa1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.sayfa1_TableAdapter = new Example_sql_project.okulDataSetTableAdapters.Sayfa1_TableAdapter();
+            this.kisiNoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.adDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.soyadDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.meslekDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sehirDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.maasDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.okulDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sayfa1BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.kisiNoDataGridViewTextBoxColumn,
+            this.adDataGridViewTextBoxColumn,
+            this.soyadDataGridViewTextBoxColumn,
+            this.meslekDataGridViewTextBoxColumn,
+            this.sehirDataGridViewTextBoxColumn,
+            this.maasDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.sayfa1BindingSource;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(645, 431);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // okulDataSet
+            // 
+            this.okulDataSet.DataSetName = "okulDataSet";
+            this.okulDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // sayfa1BindingSource
+            // 
+            this.sayfa1BindingSource.DataMember = "Sayfa1$";
+            this.sayfa1BindingSource.DataSource = this.okulDataSet;
+            // 
+            // sayfa1_TableAdapter
+            // 
+            this.sayfa1_TableAdapter.ClearBeforeFill = true;
+            // 
+            // kisiNoDataGridViewTextBoxColumn
+            // 
+            this.kisiNoDataGridViewTextBoxColumn.DataPropertyName = "KisiNo";
+            this.kisiNoDataGridViewTextBoxColumn.HeaderText = "KisiNo";
+            this.kisiNoDataGridViewTextBoxColumn.Name = "kisiNoDataGridViewTextBoxColumn";
+            // 
+            // adDataGridViewTextBoxColumn
+            // 
+            this.adDataGridViewTextBoxColumn.DataPropertyName = "Ad";
+            this.adDataGridViewTextBoxColumn.HeaderText = "Ad";
+            this.adDataGridViewTextBoxColumn.Name = "adDataGridViewTextBoxColumn";
+            // 
+            // soyadDataGridViewTextBoxColumn
+            // 
+            this.soyadDataGridViewTextBoxColumn.DataPropertyName = "Soyad";
+            this.soyadDataGridViewTextBoxColumn.HeaderText = "Soyad";
+            this.soyadDataGridViewTextBoxColumn.Name = "soyadDataGridViewTextBoxColumn";
+            // 
+            // meslekDataGridViewTextBoxColumn
+            // 
+            this.meslekDataGridViewTextBoxColumn.DataPropertyName = "Meslek";
+            this.meslekDataGridViewTextBoxColumn.HeaderText = "Meslek";
+            this.meslekDataGridViewTextBoxColumn.Name = "meslekDataGridViewTextBoxColumn";
+            // 
+            // sehirDataGridViewTextBoxColumn
+            // 
+            this.sehirDataGridViewTextBoxColumn.DataPropertyName = "Sehir";
+            this.sehirDataGridViewTextBoxColumn.HeaderText = "Sehir";
+            this.sehirDataGridViewTextBoxColumn.Name = "sehirDataGridViewTextBoxColumn";
+            // 
+            // maasDataGridViewTextBoxColumn
+            // 
+            this.maasDataGridViewTextBoxColumn.DataPropertyName = "Maas";
+            this.maasDataGridViewTextBoxColumn.HeaderText = "Maas";
+            this.maasDataGridViewTextBoxColumn.Name = "maasDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 431);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.okulDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sayfa1BindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private okulDataSet okulDataSet;
+        private System.Windows.Forms.BindingSource sayfa1BindingSource;
+        private okulDataSetTableAdapters.Sayfa1_TableAdapter sayfa1_TableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn kisiNoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn adDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn soyadDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn meslekDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sehirDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn maasDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ Example-sql-project/Example-sql-project/Form2.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace Example_sql_project
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        //SqlConnection baglanti = new SqlConnection("Data Source = LAPTOP - EL1FCCD2\\SQLEXPRESS;Initial Catalog = okul; Integrated Security = True");
+
+        SqlConnection baglanti =new SqlConnection("Server=.\\SQLEXPRESS;Database=okul;Integrated Security = True");
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // verileri yenilemek için button 1 i çalıştırıp verigetir fonksiyonunu kapatabiliriz.
+
+            //baglanti.Open();
+            //string Select = "Select * From personel";
+            //SqlDataAdapter da = new SqlDataAdapter(Select, baglanti);
+            //DataTable dt = new DataTable();
+            //da.Fill(dt);
+            //dataGridView1.DataSource = dt;
+            //baglanti.Close();
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+              //button 2 çaışır durumda        
+
+            try
+            {
+                baglanti.Open();
+
+                string save = "INSERT INTO personel (KisiNo,Ad,Soyad,Meslek,Sehir,Maas) VALUES (@p1,@p2,@p3,@p4,@p5,@p6)";
+                SqlCommand cmd = new SqlCommand(save, baglanti);
+                cmd.Parameters.AddWithValue("@p1", textBox1.Text);
+                cmd.Parameters.AddWithValue("@p2", textBox2.Text);
+                cmd.Parameters.AddWithValue("@p3", textBox3.Text);
+                cmd.Parameters.AddWithValue("@p4", textBox4.Text);
+                cmd.Parameters.AddWithValue("@p5", textBox5.Text);
+                cmd.Parameters.AddWithValue("@p6", textBox6.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("kayıt edildi.");
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+                textBox5.Text = "";
+                textBox6.Text = "";
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+
+
+        private void verigetir()
+        {
+            try
+            {
+                baglanti.Open();
+                string Select = "Select * From personel";
+                SqlDataAdapter da = new SqlDataAdapter(Select, baglanti);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+                baglanti.Close();
+            }
+            catch (Exception ex)
+            {
+
+               MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            verigetir();
+        }
+
+
+    }
+}
